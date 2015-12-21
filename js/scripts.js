f4b1719d333f7291b807f9eed4a2381f3426198f
Cindy Van <cindyvvan@gmail.com>
2015-12-20 16:57:01 -0800

    Created box and switch out images

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 $(".box-thingy .box-close").on("click", function() {
 	$(".box-thingy").addClass("collapse")
-})
+});
 
 $(".panel").on("click", function() {
 	$(".box-thingy").removeClass("collapse")
-})
+});
+
+function showBottle(id) {
+	$(".bottle img").addClass("fade")
+
+
+	setTimeout(function() {
+		$(".bottle img").attr("src", "img/" + id + ".jpg")
+	}, 300);
+
+
+	setTimeout(function() {
+		$(".bottle img").removeClass("fade");
+	}, 500);	
+}
